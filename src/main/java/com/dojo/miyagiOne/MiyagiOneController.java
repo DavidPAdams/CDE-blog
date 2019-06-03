a045fa0c495f0ecbf09a955f87a9c1760f95c7e3
David Adams <david.riding.rails@gmail.com>
2019-06-03 16:43:40 -0700

    Controller warning from sts

--- src/main/java/com/dojo/miyagiOne/MiyagiOneController.java
+++ src/main/java/com/dojo/miyagiOne/MiyagiOneController.java
@@ -2,7 +2,7 @@ package com.dojo.miyagiOne;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MiyagiOneController {
